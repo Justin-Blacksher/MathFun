7523ac530d7ad426e236aa210330c6e1a2d5a74c
Justin Blacksher <92361451+Justin-Blacksher@users.noreply.github.com>
2021-11-12 12:17:17 -0700

    Added some more Info to the constants.py
    PI
    GOLDEN_MEAN

--- constants.py
+++ constants.py
@@ -41,6 +41,33 @@ def goldenMean_help():
 def goldenMean():
     return (cm.sqrt(5) - 1) / 2
 #
+# todo: Add some use case scenarios
 #
+# -------------------------------------------------------------------------
+
+# ------------------------ Logarithm --------------------------
+#  I feel like this may turn into a personal math journal
+#  I never REALLY knew what the heck this thing was but now I am
+#  Finding myself watching videos to make sure I fully understand
+#  Pretty cool
 #
+#  I'll add a function to get the full number.
+#
+#  Interestingly upon researching to get a better understanding I had
+#  A mind blowing realization of what the heck log functions are.
+#  Pretty cool. Although I just had to delete a bunch of stuff you will
+#  Never get to see. I may turn this into a volume of ways to use log
+#  Functions and use them to express their exponential equivalents.
+#  Who knows really.
+
+def log_help(): # todo: Write a detailed usage help function.
+    return "As of now the log function will perform as a story problem for clarity"
+
+def log(x, base): # todo: Figure out how to get a better output.
+    return cm.log(x, base)
+
+# I tried to write my own function and failed miserably. Going to use this one
+# For now and get an idea using some testing to get a better output and use my
+# own function.
+
 # -------------------------------------------------------------------------
